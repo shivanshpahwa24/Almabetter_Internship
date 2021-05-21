1ec9b71d03825a01cf100abee84979ba284e30b9
Shivansh <shivanshpahwa24@gmail.com>
2021-05-21 19:42:17 +0530

    README added

--- config/db.js
+++ config/db.js
@@ -8,11 +8,4 @@ const { SQL_DB, SQL_USER, SQL_PASSWORD, SQL_TYPE, SQL_HOST } = process.env;
 module.exports = new Sequelize(SQL_DB, SQL_USER, SQL_PASSWORD, {
   host: SQL_HOST,
   dialect: SQL_TYPE,
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
 });
