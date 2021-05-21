7c5a17ffff593fc8504a9792681dc2938dfc6c0a
Shivansh <shivanshpahwa24@gmail.com>
2021-05-21 20:09:01 +0530

    Port env added

--- config/db.js
+++ config/db.js
@@ -8,4 +8,5 @@ const { SQL_DB, SQL_USER, SQL_PASSWORD, SQL_TYPE, SQL_HOST } = process.env;
 module.exports = new Sequelize(SQL_DB, SQL_USER, SQL_PASSWORD, {
   host: SQL_HOST,
   dialect: SQL_TYPE,
+  port: SQL_PORT,
 });
